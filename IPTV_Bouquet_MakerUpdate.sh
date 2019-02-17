fe5fbc822b34cd4eb0bc1a88c5497c040ef55b22
davesayers2014 <davesayers2012@gmail.com>
2019-02-17 23:49:53 +0000

    Update IPTV_Bouquet_MakerUpdate.sh

--- IPTV_Bouquet_MakerUpdate.sh
+++ IPTV_Bouquet_MakerUpdate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget --no-check-certificate -O /tmp/enigma2-plugin-extensions-e2m3u2bouquet_0.8.2_all.ipk "https://github.com/su1s/e2m3u2bouquet/releases/download/v0.8.2/enigma2-plugin-extensions-e2m3u2bouquet_0.8.2_all.ipk" && opkg install --force-reinstall /tmp/enigma2-plugin-extensions-e2m3u2bouquet_0.8.2_all.ipk
+wget -O /tmp/enigma2-plugin-extensions-e2m3u2bouquet_0.8.2_all.ipk "https://github.com/su1s/e2m3u2bouquet/releases/download/v0.8.2/enigma2-plugin-extensions-e2m3u2bouquet_0.8.2_all.ipk" && opkg install --force-reinstall /tmp/enigma2-plugin-extensions-e2m3u2bouquet_0.8.2_all.ipk
 reboot
